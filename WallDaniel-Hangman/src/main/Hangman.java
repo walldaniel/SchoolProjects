5bab9e1de8dbc59a4c2fd66222c4ef49472be90e
walldaniel <daniel.wall@ocsbstudent.ca>
2017-09-21 12:48:37 -0400

    started hangman

---
+++ WallDaniel-Hangman/src/main/Hangman.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Hangman {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = "¯\\_☺_/¯";
+		int life = 0; 	// How much lives left
+		
+		
+		do {
+			
+		} while(true);
+	}
+
+}
