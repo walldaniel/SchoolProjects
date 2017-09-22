36e2b5f27054eca76efa3ae98cfad8da118e4bf0
walldaniel <daniel.wall@ocsbstudent.ca>
2017-09-22 12:31:03 -0400

    finsihed hangman

--- WallDaniel-Hangman/src/main/Hangman.java
+++ WallDaniel-Hangman/src/main/Hangman.java
@@ -1,18 +1,112 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Hangman {
-	
+
+	// Takes a string and returns the char array with all alphabetical character
+	// replaced with '-'
+	public static char[] deleteLetters(String s) {
+		char[] newString = new char[s.length()];
+
+		// Iterates over string entered, if character is alphabetical replaces it with a
+		// '-', else stays the same
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isAlphabetic(s.charAt(i)))
+				newString[i] += '-';
+			else
+				newString[i] += s.charAt(i);
+		}
+
+		return newString;
+	}
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String s = "¯\\_☺_/¯";
-		int life = 0; 	// How much lives left
-		
-		
+		String hangman = "¯\\_☺_/¯";
+		int life = 0; // How much lives left
+		String[] words = { "JAZZ", "I LOVE JOJOS PIZZA", "I'M A MINION XD" };
+		String hiddenWord;
+		char[] guess;
+		char currentGuess;
+		boolean letterCorrect = false;
+		ArrayList<Character> lettersGuessed = new ArrayList<>();
+
+		// Welcome user to hangman
+		System.out.println("This program allows you to play hangman");
+
+		// Game loop, allows user to play multiple times
 		do {
+			// Choose random word
+			hiddenWord = words[ThreadLocalRandom.current().nextInt(0, words.length)];
+			guess = deleteLetters(hiddenWord);
+
+			do {
+				// Reset check to see if they entered a correct letter
+				letterCorrect = false;
+				
+				// Print out how many lives are left
+				System.out.println();
+				System.out.println(hangman.substring(0, life));
+
+				// Print out letters guessed so far and ask for new letters
+				System.out.println("Letters so far: " + new String(guess));
+				do {
+					System.out.print("Enter a new letter: ");
+					String s = in.nextLine().toUpperCase(); // Tests to see if user entered valid character
+
+					// If they only enter one letter than good, else get them to try again
+					if (s.length() != 1)
+						System.out.println("Only enter one character!");
+					else if (!Character.isAlphabetic(s.charAt(0)))
+						System.out.println("Enter an alphabetical character");
+					else if(lettersGuessed.contains(s.charAt(0))) {	// If the user has already entered that character don't take it as a guess
+						System.out.println("You have already tried entering that letter");
+					}else {
+						currentGuess = s.charAt(0);
+						break;
+					}
+				} while (true);
+				
+				// Check if any character is correct
+				for(int i = 0; i < hiddenWord.length(); i++) {
+					if(hiddenWord.charAt(i) == currentGuess) {
+						// If they got the letter correct, don't decrease their life left
+						letterCorrect = true;
+						// Replace the '-' in the guessed letters with the correct letter
+						guess[i] = currentGuess;
+					}
+				}
+				
+				// Add the current guess to the list of guessed character
+				lettersGuessed.add(currentGuess);
+				
+				// Check to see if they got a letter correct
+				if(!letterCorrect) {
+					System.out.println("Oh noes! you lost a life... D:");
+					life++;
+					// If they have lost all lives then quit loop
+					if(life > 7)
+						break;
+				}
+			} while (!hiddenWord.equals(new String(guess)));
+
+			// Display different messages if they one or lost
+			if(life > 7) {
+				System.out.println("You lost.");
+			} else {
+				System.out.println("Yay! you won..");
+				System.out.println("\nThe word was: " + hiddenWord);
+			}
 			
-		} while(true);
+			// Asks user if they want to try again
+			System.out.print("\nEnter 'y' to try another game of hangman: ");
+		} while (in.nextLine().equals("y"));
+
+		// Close the scanner
+		in.close();
 	}
 
 }
