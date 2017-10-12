0125948016eec1f37c44d30ad505c01daeba5432
walldaniel <daniel.wall@ocsbstudent.ca>
2017-10-12 12:42:19 -0400

    started customer list

---
+++ WallDaniel-CustomerList/src/main/CustomerList.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class CustomerList {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		BufferedReader fileReader; 
+		
+	}
+
+}
