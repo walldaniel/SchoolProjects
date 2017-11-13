275302cf52e0170177581e68cea8e5c818d6612e
Daniel_Wall <daniel.wall@ocsbstudent.ca>
2017-11-13 18:01:24 -0500

    hacker earth stuff

---
+++ Test/src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Test/src/test.java
+++ Test/src/test.java
@@ -1,66 +1,131 @@
-import java.util.Scanner;
 
-/*
- * Name: Daniel Wall
- * Date: 9-18-2017
- * Purpose: Displays 8 queens on a chest board.
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.Math;
+import java.util.Arrays;
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
 public class test {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static long getChunk(int chunk, int num) {
+		// Get rid of numbers before
+		num %= Math.pow(10, chunk * 5);
+		// Get rid of numbers after chunk
+		num /= Math.pow(10, (chunk - 1) * 5);
 
-		// Chestboard array
-		int[][] chessBoard = new int[8][8];
-
-		// Tell user what this program does
-		System.out.println("This program displays the location of 8 queens on a chess board (8x8)\n");
-
-		for (int i = 0; i < 8; i++) {
-			int x, y; // Vars for coordinates of current queen
-
-			// Loop until the user enters a valid location
-			do {
-				try {
-					// Ask user for where the queens are
-					System.out.print("Enter the coordinates of the next queen (\"x,y\"): ");
-
-					// Get input from user and parse for two numbers
-					String[] coords = in.nextLine().split(",");
-					x = Integer.parseInt(coords[0]);
-					y = Integer.parseInt(coords[1]);
-
-					// Check if they already entered that location
-					if (chessBoard[x - 1][y - 1] == 0) {
-						// Set the location on board to a queen
-						// Start the board at 1,1
-						chessBoard[x - 1][y - 1] = 1;
-
-						// Break out of loop
-						break;
-					} else {
-						System.out.println("You already entered that location");
-					}
-				} catch (NumberFormatException e) {
-					System.out.println("\nYou did not enter a correct set of coordinates");
-					System.out.println("Type first number, comma, then second number(no spaces or brackets)\n");
-				} catch (ArrayIndexOutOfBoundsException e) {
-					System.out.println("You entered a location that is not on the board");
-				}
-			} while (true);
-		}
-
-		// Print out board
-		for (int x = 0; x < chessBoard.length; x++) {
-			for (int y = 0; y < chessBoard[x].length; y++) {
-				System.out.print(chessBoard[x][y] + " ");
-			}
-			// Add a line for formatting
-			System.out.println();
-		}
-
-		// Close the scanner
-		in.close();
+		return num;
 	}
 
+	public static String arrayToString(int[] arr) {
+		if (arr == null)
+			return "null";
+		int iMax = arr.length - 1;
+		if (iMax == -1)
+			return "[]";
+
+		StringBuilder b = new StringBuilder();
+		for (int i = 0;; i++) {
+			b.append(arr[i] + " ");
+			if (i == iMax)
+				return b.toString();
+		}
+	}
+
+	public static void mergesort(int[] arr, int low, int high, int chunks) {
+        // check if low is smaller than high, if not then the array is sorted
+        if (low < high) {
+            // Get the index of the element which is in the middle
+            int middle = low + (high - low) / 2;
+            // Sort the left side of the array
+            mergesort(arr, low, middle, chunks);
+            // Sort the right side of the array
+            mergesort(arr, middle + 1, high, chunks);
+            // Combine them both
+            merge(arr, low, middle, high, chunks);
+        }
+    }
+	
+	public static void merge(int[] arr, int low, int middle, int high, int chunks) {
+		int[] helper = new int[arr.length];
+		
+        // Copy both parts into the helper array
+        for (int i = low; i <= high; i++) {
+            helper[i] = arr[i];
+        }
+
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+        // Copy the smallest values from either the left or the right side back
+        // to the original array
+        while (i <= middle && j <= high) {
+            if (getChunk(chunks, helper[i]) <= getChunk(chunks, helper[j])) {
+                arr[k] = helper[i];
+                i++;
+            } else {
+                arr[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+        // Copy the rest of the left side of the array into the target array
+        while (i <= middle) {
+            arr[k] = helper[i];
+            k++;
+            i++;
+        }
+        // Since we are sorting in-place any leftover elements from the right side
+        // are already at the right position.
+
+    }
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		String[] lines = br.readLine().split(" ");
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(lines[i]);
+		}
+
+		double time;
+		
+		boolean nextChunk;
+		int chunk = 1;
+		do {
+			nextChunk = false;
+			time = System.currentTimeMillis();
+			mergesort(a, 0, a.length - 1, chunk);
+			System.out.println(System.currentTimeMillis() - time);
+			// for(int i = 0; i < n; i++) {
+			// int temp = a[i];
+			// int j = i;
+			//
+			// while(j > 0 && getChunk(chunk, temp) < getChunk(chunk, a[j - 1])) {
+			// a[j] = a[j - 1];
+			// j--;
+			// }
+			// a[j] = temp;
+			// }
+
+			// print out number
+			time = System.currentTimeMillis();
+			System.out.println(arrayToString(a));
+			System.out.println(System.currentTimeMillis() - time);
+
+			// Check if the numbers have digits in the next chunk
+			time = System.currentTimeMillis();
+			chunk++;
+			for (int i : a) {
+				if (getChunk(chunk, i) > 0) {
+					nextChunk = true;
+					break;
+				}
+			}
+			System.out.println(System.currentTimeMillis() - time);
+		} while (nextChunk);
+	}
 }
